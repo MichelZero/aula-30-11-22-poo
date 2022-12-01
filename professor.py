8e4eaddf98b9f29f25294e2e49837aa4f1d3e2db
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-01 08:22:12 -0300

    up

--- professor.py
+++ professor.py
@@ -3,21 +3,26 @@
 #
 # data: 30/11/2022
 
+# criar uma classe Professor com os atributos nome e ferramenta.
+# ferramenta é um objeto da classe Caneta ou Computador.
+# a classe professor de iniciar com o atributo ferramenta como None e 
+# nome vinculado ao parâmetro nome.
+
 class Professor:
   def __init__(self, nome) -> None:
     self.__nome = nome
-    self.__ferramenta = None
+    self.__ferramenta = None # atributo ferramenta como None
   
   #gets
   @property
-  def nome(self):
-    return self.__nome
+  def nome(self): # get nome 
+    return self.__nome # retorna o nome do professor 
   
   @property
-  def ferramenta(self):
-    return self.__ferramenta
+  def ferramenta(self): # get ferramenta
+    return self.__ferramenta # retorna o objeto da classe Caneta ou Computador 
   
   #setters
-  @ferramenta.setter
-  def ferramenta(self, valor):
-    self.__ferramenta = valor
+  @ferramenta.setter # set ferramenta 
+  def ferramenta(self, valor): # recebe um objeto da classe Caneta ou Computador
+    self.__ferramenta = valor # atribui o objeto da classe Caneta ou Computador ao atributo ferramenta
