b5d463b78dd90a9f4711b51f2abc98a037117baf
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-01 08:23:22 -0300

    up

--- computador.py
+++ computador.py
@@ -3,14 +3,16 @@
 #
 # data: 30/11/2022
 
+# criar a classe Computador com o editor de texto.
+
 class Computador:
   def __init__(self, editor) -> None:
     self.__editor = editor
     
   #get
-  @property
-  def editor(self):
-    return self.__editor
+  @property 
+  def editor(self): # get editor
+    return self.__editor # retorna o editor de texto
   
-  def escrever(self):
-    print("Editor está escrevendo...")
+  def escrever(self): # método escrever 
+    print("Editor está escrevendo...") # escrevendo no editor de texto
