a8a6f2ae9a2728768e7a3ffc34c4e0270e3776c0
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-01 08:24:09 -0300

    up

--- caneta.py
+++ caneta.py
@@ -3,14 +3,16 @@
 #
 # data: 30/11/2022
 
+# criar a classe Caneta com a marca. 
+
 class Caneta:
   def __init__(self, marca) -> None:
-    self.__marca = marca
+    self.__marca = marca # atributo marca vinculado ao parâmetro marca
     
   #get
   @property
-  def marca(self):
-    return self.__marca
+  def marca(self): # get marca
+    return self.__marca # retorna a marca da caneta
   
-  def escrever(self):
-    return "escrevendo com caneta..."
+  def escrever(self): # método escrever 
+    return "escrevendo com caneta..." # escrevendo com a caneta 
